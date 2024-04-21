7bb70f4a2648af975ed3402617349bb62c175d51
siddikisahi47 <siddikisahil47@gmail.com>
2024-04-22 00:22:15 +0530

    Create Frontend

---
+++ Frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImageUpload from '../imageUpload/ImageUpload';
+
+function Home() {
+    return (
+        <div className="landing-page">
+            <ImageUpload />
+        </div>
+    );
+}
+
+export default Home
